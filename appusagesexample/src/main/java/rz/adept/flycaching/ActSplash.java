a425fba30def1949f8e0157ddc1e2b98b75127ee
Rz Rasel <Rz Rasel>
2018-09-28 19:14:57 +0600

    And Fly Caching V-100.00.01

--- appusagesexample/src/main/java/rz/adept/flycaching/ActSplash.java
+++ appusagesexample/src/main/java/rz/adept/flycaching/ActSplash.java
@@ -3,11 +3,14 @@ package rz.adept.flycaching;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import rz.logwriter.LogWriter;
+
 public class ActSplash extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.act_splash);
+        LogWriter.Log("Hi");
     }
 }
